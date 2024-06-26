14daac4e8c7972610a9a2d4507d72862b2fddca8
Thamjith Thaha <jithuthaha8@gmail.com>
2024-06-27 00:33:09 +0530

    ts no check added for temporary

--- src/models/bird/Bird.tsx
+++ src/models/bird/Bird.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { useAnimations, useGLTF } from '@react-three/drei';
 import { useEffect, useRef } from 'react';
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,8 @@
+// @ts-nocheck
+
 import './Home.scss';
+
+import { Suspense, useState } from 'react';
 
 import Bird from '../../models/bird/Bird';
 import { Canvas } from '@react-three/fiber';
@@ -6,9 +10,24 @@ import Island from '../../models/island/Island';
 import Loader from '../../components/loader/Loader';
 import Plane from '../../models/plane/Plane';
 import Sky from '../../models/sky/Sky';
-import { Suspense } from 'react';
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+  const adjustBiplaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
   const adjustIslandForScreenSize = () => {
     let screenScale, screenPosition;
 
@@ -23,6 +42,7 @@ const Home = () => {
     return [screenScale, screenPosition];
   };
 
+  const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
   const [islandScale, islandPosition] = adjustIslandForScreenSize();
 
   return (
@@ -50,7 +70,12 @@ const Home = () => {
             position={islandPosition}
             rotation={[0.1, 4.7077, 0]}
           />
-          <Plane />
+          <Plane
+            isRotating={isRotating}
+            position={biplanePosition}
+            rotation={[0, 20.1, 0]}
+            scale={biplaneScale}
+          />
         </Suspense>
       </Canvas>
     </section>
