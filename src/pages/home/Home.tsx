9bb9936b8a7ffda13b7470930c1f87719b3dac66
Thamjith Thaha <jithuthaha8@gmail.com>
2024-07-03 01:10:28 +0530

    Add dark mode (#4)
    * Dark theme added with material UI
    
    * Added material ui speedial and important links
    
    ---------
    
    Co-authored-by: Thamjith Thaha <thamjith.thaha@ibm.com>

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,31 @@
 import './Home.scss';
 
 import { Canvas } from '@react-three/fiber';
+import DescriptionIcon from '@mui/icons-material/Description';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Loader from '../../components/loader/Loader';
 import { OrbitControls } from '@react-three/drei';
 import SpaceMan from '../../models/spaceman/SpaceMan';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import { Suspense } from 'react';
+import { useTheme } from '@mui/material/styles';
+
+const actions = [
+  { icon: <DescriptionIcon />, name: 'Resume', href: "https://thamjiththaha.com/resume/thamjiththaha.pdf" },
+  { icon: <LinkedInIcon />, name: 'LinkedIn', href: "https://www.linkedin.com/in/thamjith-thaha/" },
+  { icon: <InstagramIcon />, name: 'Instagram', href: "https://www.instagram.com/thamjith.thaha/" }
+];
 
 const Home = () => {
+  const theme = useTheme();
 
+  const handleClick = (link: string) => {
+    window.open(link, '_blank');
+  };
+  
   return (
     <section className="home__container">
       <Canvas className="home__canvas" camera={{ near: 0.1, far: 1000 }}>
@@ -30,6 +48,31 @@ const Home = () => {
           <OrbitControls enablePan={false} enableZoom={true} />
         </Suspense>
       </Canvas>
+      <SpeedDial
+        ariaLabel="Shortcut"
+        sx={{ 
+          position: 'absolute', 
+          bottom: 16, 
+          right: 16,
+          '& .MuiSpeedDial-fab': {
+            bgcolor: theme.palette.mode === 'dark' ? 'white' : 'black', 
+          },
+          '& .MuiSpeedDial-fab:hover': {
+            bgcolor: theme.palette.mode === 'dark' ? 'white' : 'black', 
+          },
+        }}
+        icon={<SpeedDialIcon />}
+        className='home__speedial'
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={() => handleClick(action.href)}
+          />
+        ))}
+      </SpeedDial>
     </section>
   );
 };
