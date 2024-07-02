dca7932ad7a54eba42152e6f641be261b3580001
Thamjith Thaha <jithuthaha8@gmail.com>
2024-07-02 23:12:01 +0530

    Add spaceman (#3)
    * Added space man and replaced the islan
    
    * Clean up
    
    ---------
    
    Co-authored-by: Thamjith Thaha <thamjith.thaha@ibm.com>

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,48 +1,12 @@
 import './Home.scss';
 
-import { Suspense, useState } from 'react';
-
-import Bird from '../../models/bird/Bird';
 import { Canvas } from '@react-three/fiber';
-import Island from '../../models/island/Island';
 import Loader from '../../components/loader/Loader';
-import Plane from '../../models/plane/Plane';
-import Sky from '../../models/sky/Sky';
+import { OrbitControls } from '@react-three/drei';
+import SpaceMan from '../../models/spaceman/SpaceMan';
+import { Suspense } from 'react';
 
 const Home = () => {
-  const [isRotating, setIsRotating] = useState(false);
-
-  const adjustBiplaneForScreenSize = () => {
-    let screenScale, screenPosition;
-
-    // If screen width is less than 768px, adjust the scale and position
-    if (window.innerWidth < 768) {
-      screenScale = [1.5, 1.5, 1.5];
-      screenPosition = [0, -1.5, 0];
-    } else {
-      screenScale = [3, 3, 3];
-      screenPosition = [0, -4, -4];
-    }
-
-    return [screenScale, screenPosition];
-  };
-
-  const adjustIslandForScreenSize = () => {
-    let screenScale, screenPosition;
-
-    if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
-      screenPosition = [0, -6.5, -43.4];
-    } else {
-      screenScale = [1, 1, 1];
-      screenPosition = [0, -6.5, -43.4];
-    }
-
-    return [screenScale, screenPosition];
-  };
-
-  const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
-  const [islandScale, islandPosition] = adjustIslandForScreenSize();
 
   return (
     <section className="home__container">
@@ -62,21 +26,8 @@ const Home = () => {
             groundColor="#000000"
             intensity={1}
           />
-          <Bird />
-          <Sky isRotating={isRotating} />
-          <Island
-            isRotating={isRotating}
-            setIsRotating={setIsRotating}
-            position={islandPosition}
-            rotation={[0.1, 4.7077, 0]}
-            scale={islandScale}
-          />
-          <Plane
-            isRotating={isRotating}
-            position={biplanePosition}
-            rotation={[0, 20.1, 0]}
-            scale={biplaneScale}
-          />
+          <SpaceMan />
+          <OrbitControls enablePan={false} enableZoom={true} />
         </Suspense>
       </Canvas>
     </section>
