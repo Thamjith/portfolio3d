a14bf36955e7295f9b53575456dc7900d2c365ef
Thamjith Thaha <thamjith.thaha@ibm.com>
2024-07-02 23:10:41 +0530

    Added space man and replaced the islan

---
+++ src/models/spaceman/SpaceMan.tsx
@@ -0,0 +1,24 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+
+import { Mesh } from "three";
+// @ts-expect-error | Does .glb supports type defenition? Needs R&D.
+import spacemanScene from '../../assets/3d/spaceman.glb';
+
+const SpaceMan = () => {
+    const spacemanRef = useRef<Mesh>(null);
+    const { scene, animations } = useGLTF(spacemanScene);
+    const { actions } = useAnimations(animations, spacemanRef);
+
+    useEffect(() => {
+        actions["Idle"]?.play();
+    }, [actions]);
+
+    return (
+        <mesh ref={spacemanRef} rotation={[2, .5, -1]}>
+            <primitive object={scene} />
+        </mesh>
+    )
+}
+
+export default SpaceMan
